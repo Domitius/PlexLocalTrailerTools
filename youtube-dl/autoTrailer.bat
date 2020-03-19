03a08083df59d8692093fed9eb85dcf972a97839
Gavin Burris <domitius.rc@gmail.com>
2020-03-19 18:24:08 -0500

    Update autoTrailer.bat

--- youtube-dl/autoTrailer.bat
+++ youtube-dl/autoTrailer.bat
@@ -5,21 +5,21 @@ ECHO RUNNING PLEX LOCAL TRAILER DOWNLOAD ON %mydate%  :   %mytime%
 ECHO DOWNLOADING...
 ECHO ----------------------------------------------
 for /d %%i in ("C:\Users\John Doe\Desktop\sandboxTest\*") do (
-  :: Go into movie folder
-  CD "%%i"
+	:: Go into movie folder
+    CD "%%i"
 	if not exist Trailers (
-    :: move back to the youtube folder with youtube-dl
+        :: move back to the youtube folder with youtube-dl
 		CD "..\youtube-dl"
-    ECHO.
-    ECHO -------------------------------------------------------
-    ECHO Downloading %%~NXi Trailer
-    ECHO -------------------------------------------------------
-    ECHO.
-    :: Download first YouTube result from <movie folder title> Trailer
-		".\youtube-dlNew.exe" "ytsearch1:%%~NXi Trailer"
-    :: Make Trailers folder for the movie folder
+        ECHO.
+        ECHO -------------------------------------------------------
+        ECHO Downloading %%~NXi Trailer
+        ECHO -------------------------------------------------------
+        ECHO.
+        :: Download first YouTube result from <movie folder title> Trailer
+		".\youtube-dl.exe" "ytsearch1:%%~NXi Trailer"
+        :: Make Trailers folder for the movie folder
 		MKDIR "%%i\Trailers"
-    :: Move freshly downloaded trailer into new Trailers folder
+        :: Move freshly downloaded trailer into new Trailers folder
 		MOVE "*.mp4" "%%i\Trailers\Theater Trailer #1.mp4"
 	)	
 )
